c66121e893a636f1f52354ad1061abad4c8b8a7e
zhid0399123 <zhid0399123@gmail.com>
2023-12-13 14:28:39 +0300

    refactor(collection): refactored code to a javascript class

--- src/server/services/collection.js
+++ src/server/services/collection.js
@@ -1,59 +1,66 @@
 const mongodb = require("./mongodb");
 
-const getExtrackerCollection = async (collection) => {
-  const db = await mongodb.getExtrackerDatabase();
-  return db.collection(collection);
-};
-
-async function insertExercise(exercise) {
-  const exercisesCollection = await getExtrackerCollection("exercises");
-  const { acknowledged, insertedId } = await exercisesCollection.insertOne(exercise);
-  if (!acknowledged) {
-    await mongodb.closeMongoClient();
-    const error = new Error("Write result not aknowledged :)");
-    throw error;
+/** Class containing static method to ease read and write operation in mongodb database, using MongoClient driver
+ *```js
+ * 1 Collection.insertDocument(collectionName, document)
+ * 2 Collection.getDocument(collectionName, filter, options)
+ * 3 Collection.getDocuments(collectionName, filter, options)
+ * ```
+ * */
+class Collection {
+  constructor(collectionName) {
+    this.collection = async function () {
+      const db = await mongodb.getExtrackerDatabase();
+      return db.collection(collectionName);
+    };
   }
-  await mongodb.closeMongoClient();
-  return insertedId.toString();
-}
 
-async function getExercise(exerciseId) {
-  const exercisesCollection = await getExtrackerCollection("exercises");
-  const exerciseDoc = await exercisesCollection.findOne({ exerciseId }, { projection: { _id: 0 } });
-  await mongodb.closeMongoClient();
-  return exerciseDoc;
-}
-
-async function getExercises(from, to, limit) {
-  const exercisesCollection = await getExtrackerCollection("exercises");
-  const option = {
-    limit: limit,
-    min: from,
-    max: to,
-  };
-  const exercisesCursor = exercisesCollection.find({}, option);
-  const exercises = await exercisesCursor.toArray();
-  await mongodb.closeMongoClient();
-  return exercises;
-}
-
-async function insertUser({ username, password }) {
-  const usersCollection = await getExtrackerCollection("users");
-  const { acknowledged, insertedId } = await usersCollection.insertOne({ username, password });
-  if (!acknowledged) {
+  /**Insert document in database collection
+   * @param {string} collectionName - Collection name
+   * @param {object} document - Document to insert
+   * @returns {Promise<string>} `InsertedId`
+   */
+  static async insertDocument(collectionName, document) {
+    const collection = await new Collection(collectionName).collection();
+    const { acknowledged, insertedId } = await collection.insertOne(document);
+    if (!acknowledged) {
+      await mongodb.closeMongoClient();
+      const error = new Error("Write result not aknowledged :)");
+      throw error;
+    }
     await mongodb.closeMongoClient();
-    const error = new Error("Write result not aknowledged :)");
-    throw error;
+    return insertedId.toString();
   }
-  await mongodb.closeMongoClient();
-  return insertedId.toString();
+
+  /**
+   * Find document from database collection
+   * @param {string} collectionName - Collection name
+   * @param {object} filter - Find query
+   * @param {object} options - Options
+   * @returns {Promise<any|null>} `Document` | `null`
+   */
+  static async getDocument(collectionName, filter, options) {
+    const collection = await new Collection(collectionName).collection();
+    const document = await collection.findOne(filter, options);
+    await mongodb.closeMongoClient();
+    return document;
+  }
+
+  /**
+   * Returns an array of documents from database collection
+   * @param {string} collectionName - Collection name
+   * @param {object} filter - Find query
+   * @param {object} options - Options
+   * @returns {any[]} `Documents array`
+   */
+  static async getDocuments(collectionName, filter = {}, options) {
+    const collection = await new Collection(collectionName).collection();
+    const documentsCursor = collection.find(filter, options);
+    const documents = await documentsCursor.toArray();
+    await mongodb.closeMongoClient();
+    return documents;
+  }
 }
 
-async function getUser(userId) {
-  const usersCollection = await getExtrackerCollection("exercises");
-  const userDoc = await usersCollection.findOne({ userId }, { projection: { _id: 0 } });
-  await mongodb.closeMongoClient();
-  return userDoc;
-}
-
-module.exports = { insertExercise, getExercise, getExercises, insertUser, getUser };
+module.exports = Collection;
+module.exports.Collection = Collection;
