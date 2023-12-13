78477e2071e2aac4864016a2359860ff3b64fddb
zhid0399123 <zhid0399123@gmail.com>
2023-12-13 23:27:40 +0300

    refactor(mongodb): refactored to a javascript class. Removed closeMongoClient()

--- src/server/services/mongodb.js
+++ src/server/services/mongodb.js
@@ -1,22 +1,37 @@
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const isProduction = process.env.NODE_ENV === "production";
-const serverApiOptions = {
-  version: ServerApiVersion.v1,
-  strict: true,
-  deprecationErrors: true,
-};
-const mongoClientOptions = isProduction ? { serverApi: serverApiOptions } : {};
-const client = new MongoClient(process.env.MONGO_URL, mongoClientOptions);
+/**
+ * Class exposing MongoClient instance and a method to access a Db
+ *
+ * ```js
+ * const MongoDbClientInstane = new MongoDbClient(false)
+ * const client = MongoDbClientInstance.client // MongoClient
+ * const Db = MongoDbClientInstance.getDatabase("extracter") //access Db
+ * ```
+ */
+class MongoDbClient {
+  #serverApiOptions = {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  };
+  #mongoClientOptions;
 
-async function getExtrackerDatabase() {
-  await client.connect();
-  return client.db("extracker");
+  constructor(isProduction) {
+    this.#mongoClientOptions = isProduction ? { serverApi: this.#serverApiOptions } : {};
+    this.client = new MongoClient(process.env.MONGO_URL, this.#mongoClientOptions);
+  }
+
+  /**
+   * Return Db instance
+   * @param {string?} databaseNme
+   */
+  async getDatabase(databaseNme = "extracter") {
+    await this.client.connect();
+    return this.client.db(databaseNme);
+  }
 }
 
-async function closeMongoClient() {
-  await client.close();
-}
-
-module.exports = { getExtrackerDatabase, closeMongoClient };
+module.exports = MongoDbClient;
+module.exports.MongoDbclient = MongoDbClient;
