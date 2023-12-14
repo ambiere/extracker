50598efc78595995cb001afba7337f721a2a8dbd
zhid0399123 <zhid0399123@gmail.com>
2023-12-14 12:09:00 +0300

    feat(auth): added /register endpoint to handle user registration
    Registers user to the database provided that username and password are provided on route/request. If user exist it fails with error code "USR_ALRD_EXIST" and if no password or username provided, fails with error code "PSD_USR_ERR". On successfully registration, userId is return as response.

---
+++ src/server/routes/auth.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const generateHash = require("../util/generateHash");
+const Collection = require("../services/collection");
+const router = Router({ strict: true });
+
+router.post("/register", async function (req, res, next) {
+  const password = req.body.password;
+  const username = req.body.username;
+
+  try {
+    if (password && username) {
+      const filter = { username };
+      const options = { projection: { _id: 0, salt: 0, hash: 0 } };
+      const user = await Collection.getDocument("users", filter, options);
+      if (user) {
+        const error = new Error("User already registered");
+        error.code = "USR_ALRD_EXIST";
+        throw error;
+      }
+      const { salt, hash } = await generateHash(password);
+      const insertedId = await Collection.insertDocument("users", { ...filter, salt, hash });
+      insertedId && res.json({ userId: insertedId });
+    } else {
+      const error = new Error("password or username required");
+      error.code = "PSD_USR_ERR";
+      throw error;
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
