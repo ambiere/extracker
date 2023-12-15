f80aeffe84dfa075ac33be2793d4a901293043a2
zhid0399123 <zhid0399123@gmail.com>
2023-12-15 15:04:56 +0300

    feat(middleware/errorHandler): handle errors thrown by our server and respond to the client

--- src/server/middleware/errorHandler.js
+++ src/server/middleware/errorHandler.js
@@ -1,4 +1,56 @@
-module.exports = function errorHandler(error, request, response, next) {
-  response.status(400).json({ error: error.message, statusCode: 400 });
+const { MongoServerError } = require("mongodb");
+
+module.exports = function errorHandler(error, req, res, next) {
+  const isServerInternalError = error.statusCode >= 500;
+  const isUsernameConflictError = error.code === "USR_ALRD_EXIST";
+  const isInvalidTimeError = error.message === "Invalid time value";
+  const isAuthError = error.code === "USR_AUTH_ERR" || error.code === "ERR_WRNG_CRED";
+  const isPasswordUsernameError = error.code === "PSD_USR_ERR" || error.code === "INVALID_CONTENT_ERR";
+
+  if (isAuthError) ErrorUtil.handleAuthError(error, res);
+  if (isServerInternalError) ErrorUtil.handleInternalError(error, req, res);
+  if (isInvalidTimeError) ErrorUtil.handleInvalidTimeError(error, res);
+  if (isUsernameConflictError) ErrorUtil.handleExistingUserError(error, res);
+  if (isPasswordUsernameError) ErrorUtil.handlePasswordUsernameError(error, res);
   next(error);
+};
+
+const ErrorUtil = {
+  handlePasswordUsernameError: (error, res) => {
+    return res.status(400).json({
+      code: error.code,
+      error: error.message,
+      statusCode: 400,
+    });
+  },
+  handleInvalidTimeError: (error, res) => {
+    return res.status(400).json({
+      code: "DATE_RANGE_ERR",
+      error: error.message,
+      info: "Required date format mm-dd-yyyy",
+      statusCode: 400,
+    });
+  },
+  handleAuthError: (error, res) => {
+    return res.status(401).json({
+      code: error.code,
+      error: error.message,
+      statusCode: 401,
+    });
+  },
+  handleExistingUserError: (error, res) => {
+    return res.status(409).json({
+      code: error.code,
+      error: error.message,
+      statusCode: 409,
+    });
+  },
+  handleInternalError: (error, req, res) => {
+    console.log();
+    req.log.error({ req, res: res, error }, error.message);
+    return res.status(500).json({
+      info: `Fatal error: contact the support team. Id: ${req.id}.`,
+      statusCode: error.statusCode,
+    });
+  },
 };
