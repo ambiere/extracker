2066167d75d9af9f1a65290d6a4a986e9dd060f9
zhid0399123 <zhid0399123@gmail.com>
2023-12-15 15:02:27 +0300

    feat(server/server): configured routers, logger and express middleware (json(), urlencoded())

--- src/server/server.js
+++ src/server/server.js
@@ -1,24 +1,35 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const compression = require("compression");
+const pinoHttp = require("pino-http");
 const cors = require("cors");
-const router = require("./routes");
+const auth = require("./routes/auth");
+const index = require("./routes/index");
+const exercises = require("./routes/exercises");
 const pkg = require("../../package.json");
 const routeNotFound = require("./middleware/routeNotFound");
 const errorHandler = require("./middleware/errorHandler");
 const path = require("path");
+const loggerOptions = require("./config/loggerOptions");
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(pinoHttp({ ...loggerOptions }));
+
 app.get("/", async (req, res) => res.json({ root: true }));
 app.get(`/v${pkg.version.at(0)}/api/assets/:file`, async (req, res) =>
   res.sendFile(path.join(__dirname, "../../", `public/assets/${req.params.file}`))
 );
 app.get(`/v${pkg.version.at(0)}/api/version`, async (req, res) => res.json({ version: pkg.version.at(0) }));
-app.use(`/v${pkg.version.at(0)}/api/`, router);
+app.use(`/v${pkg.version.at(0)}/api/auth`, auth);
+app.use(`/v${pkg.version.at(0)}/api/users`, exercises);
+app.use(`/v${pkg.version.at(0)}/api/`, index);
 
 app.use(routeNotFound);
 app.use(errorHandler);
