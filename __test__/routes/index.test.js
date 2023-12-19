14a78c582fb60a2652f35e9672d07c9b6d0f9dd7
zhid0399123 <zhid0399123@gmail.com>
2023-12-20 02:23:17 +0300

    bump: version 0.2.0 → 0.2.1

--- __test__/routes/index.test.js
+++
@@ -1,5 +0,0 @@
-const t = require("tap");
-
-t.test("Should return shortened and long url in json", async (t) => {
-  t.pass();
-});
--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,5 +1,11 @@
 const t = require("tap");
+const request = require("supertest");
+const app = require("../src/server/server");
 
 t.test("server should start", async (t) => {
-  t.pass();
+  t.plan(3);
+  const response = await request(app).get("/");
+  t.ok(response.body, "Response has body");
+  t.match(response.body, { root: true });
+  t.pass("Server is listening");
 });
