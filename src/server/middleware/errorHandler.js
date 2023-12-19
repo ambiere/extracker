fec872f6c0ccd873ec2d249a7cd1d1fc3549df89
zhid0399123 <zhid0399123@gmail.com>
2023-12-20 01:13:03 +0300

    feat(middleware/errorHandler): handle exercise not found

--- src/server/middleware/errorHandler.js
+++ src/server/middleware/errorHandler.js
@@ -2,15 +2,17 @@ const { MongoServerError } = require("mongodb");
 
 module.exports = function errorHandler(error, req, res, next) {
   const isServerInternalError = error.statusCode >= 500;
+  const isExercieNotFoundError = error.code === "EX_NOT_FOUND";
   const isUsernameConflictError = error.code === "USR_ALRD_EXIST";
   const isInvalidTimeError = error.message === "Invalid time value";
   const isAuthError = error.code === "USR_AUTH_ERR" || error.code === "ERR_WRNG_CRED";
   const isPasswordUsernameError = error.code === "PSD_USR_ERR" || error.code === "INVALID_CONTENT_ERR";
 
   if (isAuthError) ErrorUtil.handleAuthError(error, res);
-  if (isServerInternalError) ErrorUtil.handleInternalError(error, req, res);
   if (isInvalidTimeError) ErrorUtil.handleInvalidTimeError(error, res);
+  if (isServerInternalError) ErrorUtil.handleInternalError(error, req, res);
   if (isUsernameConflictError) ErrorUtil.handleExistingUserError(error, res);
+  if (isExercieNotFoundError) ErrorUtil.handleExerciseNotFoundError(error, res);
   if (isPasswordUsernameError) ErrorUtil.handlePasswordUsernameError(error, res);
   next(error);
 };
@@ -36,6 +38,13 @@ const ErrorUtil = {
       code: error.code,
       error: error.message,
       statusCode: 401,
+    });
+  },
+  handleExerciseNotFoundError: (error, res) => {
+    return res.status(404).json({
+      code: error.code,
+      error: error.message,
+      statusCode: 404,
     });
   },
   handleExistingUserError: (error, res) => {
