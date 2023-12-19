cbef6c05644881a01d03b9d9f5397ac359a8ae42
zhid0399123 <zhid0399123@gmail.com>
2023-12-20 01:19:26 +0300

    feat(service/collection): count documents from collection and return

--- src/server/services/collection.js
+++ src/server/services/collection.js
@@ -1,13 +1,6 @@
 require("dotenv").config();
 const MongoDbclient = require("./mongodb");
 
-/** Class containing static method to ease read and write operation in mongodb database, using MongoClient driver
- *```js
- * 1 Collection.insertDocument(collectionName, document)
- * 2 Collection.getDocument(collectionName, filter, options)
- * 3 Collection.getDocuments(collectionName, filter, options)
- * ```
- * */
 class Collection {
   constructor(isProduction = process.env.NODE_ENV === "production") {
     this.mongodbClient = async function () {
@@ -16,11 +9,6 @@ class Collection {
     };
   }
 
-  /**Insert document in database collection
-   * @param {string} collectionName - Collection name
-   * @param {object} document - Document to insert
-   * @returns {Promise<string>} `InsertedId`
-   */
   static async insertDocument(collectionName, document) {
     const { db, client } = await new Collection().mongodbClient();
     const collection = db.collection(collectionName);
@@ -35,13 +23,6 @@ class Collection {
     return insertedId.toString();
   }
 
-  /**
-   * Find document from database collection
-   * @param {string} collectionName - Collection name
-   * @param {object} filter - Find query
-   * @param {object} options - Options
-   * @returns {Promise<any|null>} `Document` | `null`
-   */
   static async getDocument(collectionName, filter, options) {
     const { db, client } = await new Collection().mongodbClient();
     const collection = db.collection(collectionName);
@@ -50,22 +31,16 @@ class Collection {
     return document;
   }
 
-  /**
-   * Returns an array of documents from database collection
-   * @param {string} collectionName - Collection name
-   * @param {object} filter - Find query
-   * @param {object} options - Options
-   * @returns {{documents: any[], totalDocuments: number}} Documents array and total documents in array
-   */
   static async getDocuments(collectionName, filter = {}, options) {
     const { db, client } = await new Collection().mongodbClient();
     const collection = db.collection(collectionName);
     const documentsCursor = collection.find(filter, options);
+    const count = await collection.countDocuments(filter);
     const documents = await documentsCursor.toArray();
     await documentsCursor.close();
 
     await client.close();
-    return { documents, totalDocuments: documents.length };
+    return { documents, totalDocuments: count };
   }
 }
 
