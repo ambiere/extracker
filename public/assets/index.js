1af28f0962bce2d42f0a8dd66ed8e931ae0e3c9b
zhid0399123 <zhid0399123@gmail.com>
2023-12-20 01:08:32 +0300

    feat(assets/index): updated endpoints, dynamically add action on exercise and logs form, clear the form input after 250

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,23 +1,60 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  const resourceAnchor = document.querySelector("#resource-link");
   const endpoints = document.querySelector("#endpoints");
-  if (resourceAnchor) {
+  const idInputs = document.querySelectorAll("#id-input");
+  const formElems = document.querySelectorAll("form");
+  const to = document.querySelector("#to-input");
+  const from = document.querySelector("#from-input");
+  const limit = document.querySelector("#limit-input");
+
+  Array.from(formElems).map((form) =>
+    form.addEventListener("submit", (e) => {
+      setTimeout(() => {
+        form.reset();
+      }, 250);
+    })
+  );
+
+  if (endpoints) {
     window
       .fetch("version")
       .then((res) => res.json())
       .then((version) => {
-        resourceAnchor.setAttribute("href", `shorten/neSu8VaLfU`);
-        resourceAnchor.innerHTML = `<p>/${getEndpointSegment(version)}/neSu8VaLfU</p>`;
         endpoints.innerHTML = `   
-            <p><span>POST /${getEndpointSegment(version)}</span></p>
-            <p><span>GET /${getEndpointSegment(version)}/:urlId</span></p>
+            <p><span>POST /${getEndpointSegment(version)}/auth/register</span></p>
+            <p><span>POST /${getEndpointSegment(version)}/auth/authenticate</span></p>
+            <p><span>POST /${getEndpointSegment(version)}/users/:userId/exercises</span></p>
+            <p><span>GET /${getEndpointSegment(version)}users/:userId/exercises/:exerciseId</span></p>
+            <p><span>GET /${getEndpointSegment(version)}/users/:userId/logs</span></p>
             <p><span>...</span></p>`;
       });
 
     function getEndpointSegment(obj) {
       if (obj.version !== undefined) {
-        return `v${obj.version}/api/shorten`;
+        return `v${obj.version}/api`;
       }
     }
+  }
+  if (idInputs && to && from && limit) {
+    [...Array.from(idInputs), from, to, limit].map((input) =>
+      input.addEventListener("blur", (e) => {
+        const target = e.target;
+        const inputId = target.attributes["id"].value;
+        const logForm = formElems[3];
+        const exerciseForm = formElems[2];
+        if (inputId === "id-input" && target.value != "") {
+          exerciseForm.setAttribute("action", `users/${target.value}/exercises`);
+          logForm.setAttribute("action", `users/${target.value}/logs`);
+        }
+        if (inputId === "from-input" && target.value != "") {
+          logForm.action += `?from=${target.value}`;
+        }
+        if (inputId === "to-input" && target.value != "") {
+          logForm.action += `&to=${target.value}`;
+        }
+        if (inputId === "limit-input" && target.value != "") {
+          logForm.action += `&limit=${target.value}`;
+        }
+      })
+    );
   }
 });
